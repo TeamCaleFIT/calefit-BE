25ecec588783b9e7a81f99937d7132aca20048ef
Jaehong Choe <qnalsfj@gmail.com>
2022-07-25 15:01:10 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/com/calefit/CalefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
