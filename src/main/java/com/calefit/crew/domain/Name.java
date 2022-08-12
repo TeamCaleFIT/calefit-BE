b446d5230f28089b105e74ff75e45042426776ae
vans <spentmny@nate.com>
2022-08-12 17:00:03 +0900

    Chore: Name.java 값타입 Git add 진행

---
+++ src/main/java/com/calefit/crew/domain/Name.java
@@ -0,0 +1,14 @@
+package com.calefit.crew.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class Name {
+
+    private String name;
+}
