9a91e75bfe0c48381c5459acdb0a787ba3cf48fd
Jaehong Choe <qnalsfj@gmail.com>
2022-08-15 17:07:02 +0900

    refactor: JPA Id 생성전략 통일

--- src/main/java/com/calefit/inbody/entity/Inbody.java
+++ src/main/java/com/calefit/inbody/entity/Inbody.java
@@ -7,13 +7,14 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Inbody {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/calefit/workout/entity/Term.java
+++ src/main/java/com/calefit/workout/entity/Term.java
@@ -2,12 +2,13 @@ package com.calefit.workout.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Term {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
