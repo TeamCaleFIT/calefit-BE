366aa9560846252830dad4c1911b74b80343ecb7
vans <spentmny@nate.com>
2022-08-17 21:59:48 +0900

    Refactor: searchCrewById 메소드의 가독성을 위해 파라미터 수정

--- src/main/java/com/calefit/crew/CrewController.java
+++ src/main/java/com/calefit/crew/CrewController.java
@@ -5,10 +5,7 @@ import com.calefit.crew.dto.CrewSearchByIdResponse;
 import com.calefit.crew.dto.CrewSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +20,7 @@ public class CrewController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CrewSearchByIdResponse> searchCrewById(@PathVariable Long id) {
-        return ResponseEntity.ok(crewService.searchCrewById(id));
+    public ResponseEntity<CrewSearchByIdResponse> searchCrewById(@PathVariable("id") Long crewId) {
+        return ResponseEntity.ok(crewService.searchCrewById(crewId));
     }
 }
