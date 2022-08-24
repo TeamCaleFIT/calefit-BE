e5f9d734a39989cc99a23b55498c4207117682bb
vans <spentmny@nate.com>
2022-08-24 19:45:21 +0900

    Feat: Long 타입에 맞는 Valid 애노테이션 수정

--- src/main/java/com/calefit/crew/dto/CrewCreateRequest.java
+++ src/main/java/com/calefit/crew/dto/CrewCreateRequest.java
@@ -1,16 +1,16 @@
 package com.calefit.crew.dto;
 
 import lombok.Getter;
-import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 public class CrewCreateRequest {
 
-    @NotBlank(message = "요청자의 식별자는 필수 입니다.")
+    @NotNull(message = "요청자의 식별자는 필수 입니다.")
     private Long memberId;
 
     @NotBlank(message = "이름값은 필수 입니다.")
--- src/main/java/com/calefit/crew/dto/CrewDeleteRequest.java
+++ src/main/java/com/calefit/crew/dto/CrewDeleteRequest.java
@@ -2,11 +2,11 @@ package com.calefit.crew.dto;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 public class CrewDeleteRequest {
 
-    @NotBlank(message = "요청자의 식별자는 필수 입니다.")
+    @NotNull(message = "요청자의 식별자는 필수 입니다.")
     private Long memberId;
 }
