c11b68a5631fa70e68c6024ffbec4a77324069d9
vans <spentmny@nate.com>
2022-08-31 14:57:09 +0900

    Refactor: Member 생성자 추가 및 BodyInfo 필드 애노테이션 추가
    - MemberId 테스트를 위해 생성자 추가

--- src/main/java/com/calefit/member/entity/Member.java
+++ src/main/java/com/calefit/member/entity/Member.java
@@ -28,6 +28,8 @@ public class Member extends BaseTime {
     private String email;
     private String nickname;
     private String password;
+
+    @Embedded
     private BodyInfo bodyInfo;
 
     @Embedded
@@ -48,6 +50,13 @@ public class Member extends BaseTime {
     public Member(String email,
                   String nickname,
                   String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public Member(Long id, String email, String nickname, String password) {
+        this.id = id;
         this.email = email;
         this.nickname = nickname;
         this.password = password;
