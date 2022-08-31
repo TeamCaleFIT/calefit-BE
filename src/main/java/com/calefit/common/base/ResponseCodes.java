8bd4babad841de2c830993d7bced5a6c502c739c
vans <spentmny@nate.com>
2022-08-31 14:55:51 +0900

    Refactor: ResponseCodes의 Member 메시지 필드값 추가
    - 클래스 이름 변경 ResponseCodeAndMessages -> ResponseCodes

--- src/main/java/com/calefit/common/base/ResponseCodes.java
+++ src/main/java/com/calefit/common/base/ResponseCodes.java
@@ -1,14 +1,17 @@
 package com.calefit.common.base;
 
-public enum ResponseCodeAndMessages implements CodeAndMessages {
+public enum ResponseCodes implements CodeAndMessages {
 
     //Crew
-    CREW_MODIFY_SUCCESS("CMS","수정이 완료되었습니다.");
+    CREW_MODIFY_SUCCESS("CMS","수정이 완료되었습니다."),
+
+    //Member
+    MEMBER_SEARCH_SUCCESS("MSS", "멤버 프로필 조회가 성공하였습니다.");
 
     private final String code;
     private final String message;
 
-    ResponseCodeAndMessages(String code, String message) {
+    ResponseCodes(String code, String message) {
         this.code = code;
         this.message = message;
     }
