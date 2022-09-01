8005cc5c24c5ff402e4272a29418178abbf5b1d1
Jaehong Choe <qnalsfj@gmail.com>
2022-09-01 16:42:54 +0900

    Test: Inbody 전체 조회 api 테스트코드 추가

--- src/test/java/com/calefit/inbody/InbodyServiceTest.java
+++ src/test/java/com/calefit/inbody/InbodyServiceTest.java
@@ -4,12 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import com.calefit.inbody.domain.BodyComposition;
+import com.calefit.inbody.dto.SearchInbodyResponse;
 import com.calefit.inbody.entity.Inbody;
 import com.calefit.member.MemberRepository;
 import com.calefit.member.entity.Member;
 import com.calefit.member.exception.NotFoundMemberException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -63,6 +67,65 @@ class InbodyServiceTest {
 
             then(memberRepository).should(times(1)).findById(member.getId());
             then(inbodyRepository).should(times(1)).save(newInbody);
+        }
+
+        @Test
+        void 존재하지_않는_회원id를_전달받으면_NotFoundMemberException_예외가_발생하고_인바디_등록에_실패한다() {
+            //given
+            Long notExistMemberId = 2L;
+            LocalDateTime measuredTime = LocalDateTime.of(2022, 8, 20, 13, 5);
+            BodyComposition bodyComposition = new BodyComposition(30.0, 17.5, 65.0);
+            Inbody newInbody = new Inbody(member, measuredTime, bodyComposition);
+
+            given(memberRepository.findById(notExistMemberId)).willThrow(new NotFoundMemberException());
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> inbodyService.createInbody(notExistMemberId, measuredTime, bodyComposition))
+                .isInstanceOf(NotFoundMemberException.class);
+
+            then(memberRepository).should(times(1)).findById(notExistMemberId);
+            then(inbodyRepository).should(never()).save(newInbody);
+        }
+    }
+
+    @Nested
+    @DisplayName("인바디 전체 조회시")
+    class InbodySearch {
+
+        @Test
+        void 존재하는_멤버_id를_전달받으면_멤버_확인_후_해당_멤버의_전체_인바디_정보를_조회하여_각각_dto로_변환하여_리스트에_담아_리턴한다() {
+            //given
+            LocalDateTime measuredTime1 = LocalDateTime.of(2022, 1, 20, 13, 5);
+            LocalDateTime measuredTime2 = LocalDateTime.of(2022, 2, 20, 13, 5);
+            BodyComposition bodyComposition1 = new BodyComposition(31.0, 17.5, 65.0);
+            BodyComposition bodyComposition2 = new BodyComposition(32.0, 17.5, 65.0);
+
+            Inbody inbody1 = new Inbody(member, measuredTime1, bodyComposition1);
+            Inbody inbody2 = new Inbody(member, measuredTime2, bodyComposition2);
+
+            List<Inbody> findInbodies = new ArrayList<>();
+            findInbodies.add(inbody1);
+            findInbodies.add(inbody2);
+
+            List<SearchInbodyResponse> givenInbodyResponses = findInbodies.stream()
+                .map(SearchInbodyResponse::from)
+                .collect(Collectors.toList());
+
+            given(memberRepository.existsById(member.getId())).willReturn(true);
+            given(inbodyRepository.findByMemberId(member.getId())).willReturn(findInbodies);
+
+            //when
+            List<SearchInbodyResponse> inbodyResponses = inbodyService.listInbodies(member.getId());
+
+            //then
+            assertThat(inbodyResponses)
+                .usingRecursiveComparison()
+                .isEqualTo(givenInbodyResponses);
+
+            then(memberRepository).should(times(1)).existsById(member.getId());
+            then(inbodyRepository).should(times(1)).findByMemberId(member.getId());
         }
 
         @Test
