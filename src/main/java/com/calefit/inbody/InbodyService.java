0609e0a25ca16bee59d1a2202d5ba525b2302a56
Jaehong Choe <qnalsfj@gmail.com>
2022-09-02 15:15:59 +0900

    Feat: Inbody 삭제 api 구현

--- src/main/java/com/calefit/inbody/InbodyService.java
+++ src/main/java/com/calefit/inbody/InbodyService.java
@@ -3,11 +3,12 @@ package com.calefit.inbody;
 import com.calefit.inbody.domain.BodyComposition;
 import com.calefit.inbody.dto.SearchInbodyResponse;
 import com.calefit.inbody.entity.Inbody;
+import com.calefit.inbody.exception.NotFoundInbodyException;
 import com.calefit.member.MemberRepository;
 import com.calefit.member.entity.Member;
+import com.calefit.member.exception.NotFoundMemberException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class InbodyService {
     @Transactional
     public Inbody createInbody(Long memberId, LocalDateTime measuredDateTime, BodyComposition bodyComposition) {
         Member member = memberRepository.findById(memberId)
-            .orElseThrow(NoSuchElementException::new);
+            .orElseThrow(NotFoundMemberException::new);
 
         return inbodyRepository.save(new Inbody(member, measuredDateTime, bodyComposition));
     }
@@ -31,7 +32,7 @@ public class InbodyService {
     public List<SearchInbodyResponse> listInbodies(Long memberId) {
         //todo: pk아닌 회원의 유니크한 id로 조회하도록 변경
         if (!memberRepository.existsById(memberId)) {
-            throw new NoSuchElementException("존재하지 않는 회원 id입니다.");
+            throw new NotFoundMemberException();
         }
         List<Inbody> foundInbodies = inbodyRepository.findByMemberId(memberId);
 
@@ -40,4 +41,15 @@ public class InbodyService {
             .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void deleteInbody(Long id, Long memberId) {
+        //todo: 인증은 나중에 인터셉터에서 처리
+        if (!memberRepository.existsById(memberId)) {
+            throw new NotFoundMemberException();
+        }
+        if (!inbodyRepository.existsById(id)) {
+            throw new NotFoundInbodyException();
+        }
+        inbodyRepository.deleteById(id);
+    }
 }
