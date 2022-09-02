91f8309f0fb6ef999fe8fd8d3c30e44c91bf6d5d
vans <spentmny@nate.com>
2022-09-02 17:03:24 +0900

    Feat: Member 회원가입 테스트코드 구현
    - 가상 Member id 입력을 위한 ReflectionUtils 추가
    - 추가 메소드 테스트 여부 확인을 위한 shouldHaveNoMoreInteractions 단언 추가

--- src/test/java/com/calefit/unit/member/MemberServiceTest.java
+++ src/test/java/com/calefit/unit/member/MemberServiceTest.java
@@ -3,24 +3,32 @@ package com.calefit.unit.member;
 import com.calefit.member.MemberRepository;
 import com.calefit.member.MemberService;
 import com.calefit.member.dto.MemberSearchResponse;
+import com.calefit.member.dto.MemberSignUpRequest;
 import com.calefit.member.entity.Member;
+import com.calefit.member.exception.NotAvailableMemberEmailException;
+import com.calefit.member.exception.NotAvailableMemberNicknameException;
 import com.calefit.member.exception.NotFoundMemberException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DisplayName("MemberService 단위테스트")
 class MemberServiceTest {
 
@@ -29,20 +37,24 @@ class MemberServiceTest {
     @Mock
     private MemberRepository memberRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
     private Member member;
     private Long EXCEPTION_NUMBER;
 
     @BeforeEach
     void setUp() {
-        member = new Member(1L, "2@c.com", "Vans", "1234");
+        member = new Member("2@c.com", "Vans", "1234");
+        Field idField = ReflectionUtils.findField(Member.class, "id");
+        ReflectionUtils.makeAccessible(idField);
+        ReflectionUtils.setField(Objects.requireNonNull(idField), member, 1L);
     }
 
     @Nested
-    @DisplayName("멤버 프로필 조회 클래스")
+    @DisplayName("멤버 프로필 조회시")
     class SearchMemberProfileTest {
 
         @Test
-        @DisplayName("멤버 프로필 조회 기능 테스트")
         void 멤버Id를_전달받으면_해당_멤버를_조회하고_정보를_반환한다() {
 
             //given
@@ -55,11 +67,12 @@ class MemberServiceTest {
             assertThat(searchedMemberProfile.getId()).isEqualTo(member.getId());
             assertThat(searchedMemberProfile.getEmail()).isEqualTo(member.getEmail());
             assertThat(searchedMemberProfile.getNickname()).isEqualTo(member.getNickname());
-            assertThat(searchedMemberProfile.getBodyInfo()).isNull();
+
+            then(memberRepository).should(times(1)).findById(member.getId());
+            then(memberRepository).shouldHaveNoMoreInteractions();
         }
 
         @Test
-        @DisplayName("멤버 프로필 조회 예외 테스트")
         void 존재하지_않는_멤버Id를_전달받으면_예외를_발생_시킨다() {
 
             //given
@@ -70,6 +83,67 @@ class MemberServiceTest {
 
             //then
             assertThatThrownBy(() -> memberRepository.findById(EXCEPTION_NUMBER)).isInstanceOf(NotFoundMemberException.class);
+
+            then(memberRepository).should(times(1)).findById(any());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+        }
+    }
+
+    @Nested
+    @DisplayName("멤버 회원가입 요청시")
+    class signUpMemberTest {
+
+        @Test
+        void 요청_정보를_전달받으면_멤버_회원가입을_완료한다() {
+
+            //given
+            MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest("pio@naver.com","Pio","1234");
+            given(memberRepository.existsMemberByEmail(memberSignUpRequest.getEmail())).willReturn(false);
+            given(memberRepository.existsMemberByNickname(memberSignUpRequest.getNickname())).willReturn(false);
+            doReturn(null).when(memberRepository).save(any());
+
+            //when
+            memberService.signUpMember(memberSignUpRequest);
+
+            //then
+            then(memberRepository).should(times(1)).save(any());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+        }
+
+        @Test
+        void 요청_정보중에_이메일_정보가_중복되면_예외를_발생시킨다() {
+
+            //given
+            MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest("2@c.com","Pio","1234");
+            given(memberRepository.existsMemberByEmail(memberSignUpRequest.getEmail())).willThrow(new NotAvailableMemberEmailException());
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> memberRepository.existsMemberByEmail(memberSignUpRequest.getEmail())).isInstanceOf(NotAvailableMemberEmailException.class);
+
+            then(memberRepository).should(times(1)).existsMemberByEmail(any());
+            then(memberRepository).should(never()).existsMemberByNickname(any());
+            then(memberRepository).should(never()).save(any());
+            then(memberRepository).shouldHaveNoMoreInteractions();
+        }
+
+        @Test
+        void 요청_정보중에_닉네임_정보가_중복되면_예외를_발생시킨다() {
+
+            //given
+            MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest("2@c.com","Pio","1234");
+            given(memberRepository.existsMemberByNickname(memberSignUpRequest.getNickname())).willThrow(new NotAvailableMemberNicknameException());
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> memberRepository.existsMemberByNickname(memberSignUpRequest.getNickname())).isInstanceOf(NotAvailableMemberNicknameException.class);
+
+            then(memberRepository).should(times(1)).existsMemberByNickname(any());
+            then(memberRepository).should(never()).existsMemberByEmail(any());
+            then(memberRepository).should(never()).save(any());
+            then(memberRepository).shouldHaveNoMoreInteractions();
         }
     }
 }
