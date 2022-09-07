0b50be048a95f2f31bfbfd4e020736d610f58d3c
vans <spentmny@nate.com>
2022-09-07 23:24:03 +0900

    Refactor: 불필요한 Config 삭제 및 MemberService TODO 작성

--- src/main/java/com/calefit/member/member/MemberService.java
+++ src/main/java/com/calefit/member/member/MemberService.java
@@ -20,14 +20,15 @@ public class MemberService {
 
     @Transactional(readOnly = true)
     public MemberSearchResponse searchMemberProfile(Long memberId) {
+        //TODO: 로그인한 유저와 요청하는 Member Id가 일치하는지 확인 필요
         Member searchedMember = memberRepository.findById(memberId).orElseThrow(NotFoundMemberException::new);
         return MemberSearchResponse.from(searchedMember);
     }
 
     @Transactional
     public void signUpMember(MemberSignUpRequest memberRequest) {
+        //TODO: 회원가입시, password 암호화처리 필요
         validateDuplicateMemberInfo(memberRequest.getEmail(), memberRequest.getNickname());
-
         Member member = new Member(
                 memberRequest.getEmail(),
                 memberRequest.getNickname(),
