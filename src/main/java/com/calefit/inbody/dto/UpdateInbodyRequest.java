5c1aec8c7cfaffe85148210eb484ed29c7909707
Jaehong Choe <qnalsfj@gmail.com>
2022-09-11 17:56:55 +0900

    Refactor: Inbody 수정 request dto 수정
    - PUT 방식으로 처리할 수 있도록 validation 어노테이션을 수정

--- src/main/java/com/calefit/inbody/dto/UpdateInbodyRequest.java
+++ src/main/java/com/calefit/inbody/dto/UpdateInbodyRequest.java
@@ -1,10 +1,12 @@
 package com.calefit.inbody.dto;
 
+import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.lang.Nullable;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @NoArgsConstructor
@@ -14,15 +16,20 @@ public class UpdateInbodyRequest {
     @NotNull(message = "인바디 id를 입력해주세요.")
     private Long inbodyId;
 
-    @Positive(message = "골격근량은 양수를 입력해주세요.")
-    @Nullable
+    @PastOrPresent(message = "미래의 날짜, 시간을 입력할 수 없습니다.")
+    @NotNull(message = "인바디 측정 날짜 및 시간을 입력해주세요.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") //pattern을 사용할지, iso를 사용할지?
+    private LocalDateTime measuredDateTime;
+
+    @Positive(message = "골격근량을 양수 값을 입력해주세요.")
+    @NotNull(message = "인바디 측정 날짜 및 시간을 입력해주세요.")
     private Double muscle;
 
-    @Positive(message = "체지방률은 양수를 입력해주세요.")
-    @Nullable
+    @Positive(message = "체지방률은 양수 값을 입력해주세요.")
+    @NotNull(message = "체지방률을 입력해주세요.")
     private Double bodyFat;
 
-    @Positive(message = "몸무게는 양수를 입력해주세요.")
-    @Nullable
+    @Positive(message = "몸무게는 양수 값을 입력해주세요.")
+    @NotNull(message = "몸무게를을 입력해주세요.")
     private Double bodyWeight;
 }
