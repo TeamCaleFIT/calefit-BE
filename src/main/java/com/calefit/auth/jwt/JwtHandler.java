b0d4118d3746ee04c901c9d7b5d3583b76f89c85
vans <spentmny@nate.com>
2022-10-25 17:55:46 +0900

    Feat: Token 생성시, 만료시간 단위 수정
    - Cookie의 maxage 시간단위(초)와 동일하게 하기 위해 수정
    
    Co-Authored-By: NB993 <46119135+NB993@users.noreply.github.com>

--- src/main/java/com/calefit/auth/jwt/JwtHandler.java
+++ src/main/java/com/calefit/auth/jwt/JwtHandler.java
@@ -27,7 +27,7 @@ public class JwtHandler {
                 .setIssuer(jwtProperties.getIssuer())
                 .claim("memberId", memberId)
                 .setIssuedAt(new Date())
-                .setExpiration(Date.from(Instant.now().plusMillis(expirationPeriod)))
+                .setExpiration(Date.from(Instant.now().plusSeconds(expirationPeriod)))
                 .signWith(createSecretKey())
                 .compact();
     }
@@ -36,12 +36,6 @@ public class JwtHandler {
         return Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
     }
 
-    /* todo: parseClaimsJws()에서 발생하는 예외 케이스들
-        - UnsupportedJwtException : jwt가 예상하는 형식과 다른 형식이거나 구성
-        - MalformedJwtException : 잘못된 jwt 구조
-        - ExpiredJwtException : JWT의 유효기간이 초과 -> 이 핸들러에서 리프레시 토큰을 확인해봐야 하는데 음.. 어떻게?
-        - SignatureException : JWT의 서명실패(변조 데이터)
-        위 예외 처리하도록 핸들러 추가하기 */
     public Claims decodeJwt(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(createSecretKey())
