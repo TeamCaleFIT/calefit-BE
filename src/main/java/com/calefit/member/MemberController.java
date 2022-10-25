bdd8a2b425bfcaf41ecb9878e2f7da55c46ef6fd
vans <spentmny@nate.com>
2022-10-25 17:47:06 +0900

    Refactor: 불필요한 Exception 및 Test코드 삭제
    - Member의 일반회원가입, 일반로그인 로직 제외하여 불필요한 내용들 삭제
    
    Co-Authored-By: NB993 <46119135+NB993@users.noreply.github.com>

--- src/main/java/com/calefit/member/MemberController.java
+++ src/main/java/com/calefit/member/MemberController.java
@@ -2,14 +2,13 @@ package com.calefit.member;
 
 import com.calefit.common.base.ResponseCodes;
 import com.calefit.common.entity.CommonResponseEntity;
-import com.calefit.member.dto.MemberLoginRequest;
 import com.calefit.member.dto.MemberSearchResponse;
-import com.calefit.member.dto.MemberSignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,22 +21,5 @@ public class MemberController {
     public CommonResponseEntity<MemberSearchResponse> searchMemberProfileById(@PathVariable("id") Long Id) {
         MemberSearchResponse memberSearchResponse = memberService.searchMemberProfile(Id);
         return new CommonResponseEntity<>(ResponseCodes.MEMBER_SEARCH_SUCCESS, memberSearchResponse, HttpStatus.OK);
-    }
-
-    @PostMapping("/signUp")
-    public CommonResponseEntity<Void> signUpMember(@Valid @RequestBody MemberSignUpRequest memberRequest) {
-        memberService.signUpMember(
-                memberRequest.getEmail(),
-                memberRequest.getNickname(),
-                memberRequest.getPassword());
-        return new CommonResponseEntity<>(ResponseCodes.MEMBER_SIGNUP_SUCCESS, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public CommonResponseEntity<Void> loginMember(@Valid @RequestBody MemberLoginRequest memberRequest) {
-        memberService.loginMember(
-                memberRequest.getEmail(),
-                memberRequest.getPassword());
-        return new CommonResponseEntity<>(ResponseCodes.MEMBER_LOGIN_SUCCESS, HttpStatus.OK);
     }
 }
