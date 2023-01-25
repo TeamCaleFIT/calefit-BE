59cffd80e1f424d531335472aec563a25dd0cad1
Jaehong Choe <qnalsfj@gmail.com>
2023-01-25 17:52:46 +0900

    Test: 기작성된 테스트코드도 Nested class로 변경

--- src/test/java/com/calefit/unit/workout/WorkoutServiceTest.java
+++ src/test/java/com/calefit/unit/workout/WorkoutServiceTest.java
@@ -63,56 +63,60 @@ class WorkoutServiceTest {
         ReflectionTestUtils.setField(member, "id", 1L);
     }
 
-    @Test
-    void 존재하지_않는_운동종목을_조회하면_비어있는_리스트를_리턴한다() {
-        //given
-        String notExistWorkoutName = "등록되어 있지 않은 운동";
-        List<Workout> emptyWorkouts = new ArrayList<>();
+    @Nested
+    class 운동_종목_조회시 {
+        @Test
+        void 존재하지_않는_운동_종목을_조회하면_비어있는_리스트를_리턴한다() {
+            //given
+            String notExistWorkoutName = "등록되어 있지 않은 운동";
+            List<Workout> emptyWorkouts = new ArrayList<>();
 
-        given(workoutRepository.findByNameContains(notExistWorkoutName)).willReturn(emptyWorkouts);
+            given(workoutRepository.findByNameContains(notExistWorkoutName)).willReturn(emptyWorkouts);
 
-        //when
-        List<SearchWorkoutResponse> searchWorkoutResponses = workoutService.listWorkouts(notExistWorkoutName);
+            //when
+            List<SearchWorkoutResponse> searchWorkoutResponses = workoutService.listWorkouts(notExistWorkoutName);
 
-        //then
-        assertThat(searchWorkoutResponses)
-            .isEmpty();
+            //then
+            assertThat(searchWorkoutResponses)
+                .isEmpty();
 
-        then(workoutRepository).should(times(1)).findByNameContains(notExistWorkoutName);
-        then(workoutRepository).shouldHaveNoMoreInteractions();
-    }
+            then(workoutRepository).should(times(1)).findByNameContains(notExistWorkoutName);
+            then(workoutRepository).shouldHaveNoMoreInteractions();
+        }
 
-    @Test
-    void 운동종목명에_조회하기_위한_단어가_포함되어있으면_조회결과로_리턴된다() {
-        //given
-        String someWordContainedByOverTwoWorkouts = "컬";
+        @Test
+        void 운동_종목명에_조회하기_위한_단어가_포함되어있으면_조회결과로_리턴된다() {
+            //given
+            String someWordContainedByOverTwoWorkouts = "컬";
 
-        Workout containsSearchingWord1 = new Workout("바벨 컬", "http://s3.aws.com/barbell_curl.png", "바벨 컬은 이런 운동입니다.");
-        Workout containsSearchingWord2 = new Workout("케이블 컬", "http://s3.aws.com/cable_curl.png", "케이블 컬은 이런 운동입니다.");
-        Workout notContainsSearchingWord = new Workout("스쿼트", "http://s3.aws.com/squat.png", "스쿼트는 이런 운동입니다.");
+            Workout containsSearchingWord1 = new Workout("바벨 컬", "http://s3.aws.com/barbell_curl.png", "바벨 컬은 이런 운동입니다.");
+            Workout containsSearchingWord2 = new Workout("케이블 컬", "http://s3.aws.com/cable_curl.png", "케이블 컬은 이런 운동입니다.");
+            Workout notContainsSearchingWord = new Workout("스쿼트", "http://s3.aws.com/squat.png", "스쿼트는 이런 운동입니다.");
 
-        List<Workout> workoutsWithMoreThanOneResult = new ArrayList<>();
-        workoutsWithMoreThanOneResult.add(containsSearchingWord1);
-        workoutsWithMoreThanOneResult.add(containsSearchingWord2);
+            List<Workout> workoutsWithMoreThanOneResult = new ArrayList<>();
+            workoutsWithMoreThanOneResult.add(containsSearchingWord1);
+            workoutsWithMoreThanOneResult.add(containsSearchingWord2);
 
-        List<SearchWorkoutResponse> givenWorkoutsResponse = workoutsWithMoreThanOneResult.stream()
-            .map(SearchWorkoutResponse::from)
-            .collect(Collectors.toList());
+            List<SearchWorkoutResponse> givenWorkoutsResponse = workoutsWithMoreThanOneResult.stream()
+                .map(SearchWorkoutResponse::from)
+                .collect(Collectors.toList());
 
-        given(workoutRepository.findByNameContains(someWordContainedByOverTwoWorkouts))
-            .willReturn(workoutsWithMoreThanOneResult);
+            given(workoutRepository.findByNameContains(someWordContainedByOverTwoWorkouts))
+                .willReturn(workoutsWithMoreThanOneResult);
 
-        //when
-        List<SearchWorkoutResponse> searchWorkoutResponses = workoutService.listWorkouts(
-            someWordContainedByOverTwoWorkouts);
+            //when
+            List<SearchWorkoutResponse> searchWorkoutResponses = workoutService.listWorkouts(
+                someWordContainedByOverTwoWorkouts);
 
-        //then
-        assertThat(searchWorkoutResponses)
-            .usingRecursiveComparison()
-            .isEqualTo(givenWorkoutsResponse);
+            //then
+            assertThat(searchWorkoutResponses)
+                .usingRecursiveComparison()
+                .isEqualTo(givenWorkoutsResponse);
 
-        then(workoutRepository).should(times(1)).findByNameContains(someWordContainedByOverTwoWorkouts);
-        then(workoutRepository).shouldHaveNoMoreInteractions();
+            then(workoutRepository).should(times(1)).findByNameContains(someWordContainedByOverTwoWorkouts);
+            then(workoutRepository).shouldHaveNoMoreInteractions();
+        }
+
     }
 
     @Nested
